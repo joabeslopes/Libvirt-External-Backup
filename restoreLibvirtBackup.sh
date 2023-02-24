767a330e14e69311d5ad307be89691882224cc37
Joabe Lopes <104812737+joabeslopes@users.noreply.github.com>
2023-02-24 02:18:29 -0300

    Update restoreLibvirtBackup.sh

--- restoreLibvirtBackup.sh
+++ restoreLibvirtBackup.sh
@@ -5,15 +5,15 @@
 ipBkpServ=0.0.0.0	  	        # IP of your backup server
 portBkpServer=22                        # SSH port of backup server (default: 22)
 usrBkpServ=username		        # Username of backup server
-FolderBkpServ=/backup/vms/*		# Folder of your backup server (important let the * in the end)
+FolderBkpServ=/backup/vms		# Folder of your backup server
 restoreFolder=/var/lib/libvirt/images   # Restore directly to the original place
 
-rsync -av $UsrBkpServ@$IpBkpServ:$FolderBkpServ $restoreFolder/restore
+rsync -av $UsrBkpServ@$IpBkpServ:$FolderBkpServ/* $restoreFolder/restore
 
 for vm in $restoreFolder/restore/*
 do
- 	vm=${vm##*/}
-	mv $vm/$vm.qcow2 $restoreFolder
-	virsh define $vm/$vm.xml
+        vm=${vm##*/}
+        mv $restoreFolder/restore/$vm/$vm.qcow2 $restoreFolder
+        virsh define $restoreFolder/restore/$vm/$vm.xml
 done
 rm -r $restoreFolder/restore
