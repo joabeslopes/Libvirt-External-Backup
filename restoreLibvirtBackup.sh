b04683af3344c9eb9ecbf18ef849965e7ddd6e4d
Joabe Lopes <104812737+joabeslopes@users.noreply.github.com>
2023-03-12 00:21:26 -0300

    Update restoreLibvirtBackup.sh

--- restoreLibvirtBackup.sh
+++ restoreLibvirtBackup.sh
@@ -2,18 +2,18 @@
 
 # Before do it, don't forget to reinstall all the necessary packages of virt-manager, rsync, ...
 
-ipBkpServ=0.0.0.0	  	        # IP of your backup server
-portBkpServer=22                        # SSH port of backup server (default: 22)
-usrBkpServ=username		        # Username of backup server
-FolderBkpServ=/backup/vms		# Folder of your backup server
-restoreFolder=/var/lib/libvirt/images   # Restore directly to the original place
+ipBkpSrv=0.0.0.0	  		# IP of your backup server
+portBkpSrv=22                        # SSH port of backup server (default)
+userBkpSrv=user			# Username of backup server
+folderBkpSrv=/VM/backup/folder		# Folder of your backup server
+vmFolder=/var/lib/libvirt/images		# Libvirt folder for the VM disks (default)
 
-rsync -av $UsrBkpServ@$IpBkpServ:$FolderBkpServ/* $restoreFolder/restore
+rsync -ave "ssh -p $portBkpSrv" --mkpath $userBkpSrv@$ipBkpSrv:$folderBkpSrv/* $vmFolder/restore
 
-for vm in $restoreFolder/restore/*
+for vm in $vmFolder/restore/*
 do
         vm=${vm##*/}
-        mv $restoreFolder/restore/$vm/$vm.qcow2 $restoreFolder
-        virsh define $restoreFolder/restore/$vm/$vm.xml
+        mv $vmFolder/restore/$vm/$vm.qcow2 $vmFolder
+        virsh define $vmFolder/restore/$vm/$vm.xml
 done
-rm -r $restoreFolder/restore
+rm -r $vmFolder/restore
