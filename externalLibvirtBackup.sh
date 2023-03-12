ac0a7ed85491f988c1821d5cdc449c82b80b5f20
Joabe Lopes <104812737+joabeslopes@users.noreply.github.com>
2023-03-12 00:13:41 -0300

    Update externalLibvirtBackup.sh

--- externalLibvirtBackup.sh
+++ externalLibvirtBackup.sh
@@ -5,18 +5,20 @@
 # This method also works with the vm running, but i don't know if it can really broken the file. Until now it didn´t happened to me.
 
 
-ipBkpServ=0.0.0.0	  		# IP of your backup server
-portBkpServer=22                        # SSH port of backup server (default: 22)
-usrBkpServ=username			# Username of backup server
-folderBkpServ=/folder/of/VMbackup		# Folder of your backup server
+ipBkpSrv=0.0.0.0	  		# IP of your backup server
+portBkpSrv=22                        # SSH port of backup server (default)
+userBkpSrv=user			# Username of backup server
+folderBkpSrv=/VM/backup/folder		# Folder of your backup server
+VmFolder=/var/lib/libvirt/images		# Libvirt folder for the VM disks (default)
 
-cd /var/lib/libvirt/images
+cd $VmFolder
 for vm in ./*
 do
 	vm=${vm%*.qcow2}
  	vm=${vm##*/}
 # this 2 lines are there for apply a 'filter', keeping only the vm name on the variable
+
 	virsh dumpxml $vm > $vm.xml
-	rsync -ave 'ssh -p $portBkpServer' --mkpath $vm.* $usrBkpServ@$ipBkpServ:$folderBkpServ/$vm/
+	rsync -ave "ssh -p $portBkpSrv" --mkpath $vm.* $userBkpSrv@$ipBkpSrv:$folderBkpSrv/$vm/
 rm $vm.xml
 done
